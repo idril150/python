e4969389d0333dbd589e87a9d332fbe80e6768ef
idril150 <ebibari1@gmail.com>
2024-10-31 14:24:42 -0600

    BD CRUD

--- bases_de_datos/uniqueyCrud.py
+++ bases_de_datos/uniqueyCrud.py
@@ -3,12 +3,19 @@ import sqlite3
 bdconexion=sqlite3.connect("gestionProductos")
 bdcursor=bdconexion.cursor()
 
+#bdcursor.execute("INSERT INTO PRODUCTOS VALUES (NULL, 'AUDIFONOS', 20, 'ELECTRONICA')")#CREATE
+
+bdcursor.execute("SELECT * FROM PRODUCTOS WHERE SECCION='ELECTRONICA'") #READ
+a=bdcursor.fetchall()
+print(a)
+
+bdcursor.execute("UPDATE PRODUCTOS SET PRECIO=25 WHERE ID=7")   #UPDATE DE DATOS
+
 bdcursor.execute("SELECT * FROM PRODUCTOS WHERE SECCION='ELECTRONICA'")
 a=bdcursor.fetchall()
 print(a)
 
-bdcursor.execute("UPDATE PRODUCTOS SET PRECIO=25 WHERE ID=6")   #ACTUALIZACION DE DATOS
-
+bdcursor.execute("DELETE FROM PRODUCTOS WHERE ID=08")#DELETE
 bdcursor.execute("SELECT * FROM PRODUCTOS WHERE SECCION='ELECTRONICA'")
 a=bdcursor.fetchall()
 print(a)
